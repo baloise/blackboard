4294a50014c1ed84e9dac400cadb380420a5709e
Yannick Holzenkamp <yannick.holzenkamp@baloise.ch>
2018-01-17 15:47:41 +0100

    enhance api to get specific entry

--- server/api/entry.api.js
+++ server/api/entry.api.js
@@ -1,15 +1,23 @@
 const fetch = require('node-fetch');
 const getEntries = () => {
-    return fetch(`${config.DB_URL}/entries`, {
+    return fetch(`${config.DB_URL}/entry`, {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}
     }).then(response => response.json())
       .catch(err => console.log(err));
 };
 
+const getEntry = (id) => {
+    return fetch(`${config.DB_URL}/entry/${id}`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    }).then(response => response.json())
+        .catch(err => console.log(err));
+};
+
 module.exports = {
     registerEndpoints(app) {
-        app.get('/api/entries', (req, res) =>
+        app.get('/api/entry', (req, res) =>
             getEntries()
                 .then(entries => res.json(entries))
                 .catch((err) => {
@@ -17,6 +25,15 @@ module.exports = {
                     res.sendStatus(500).send('Fetching entries failed!');
                 })
         );
+        app.get('/api/entry/:id', (req, res) =>
+            getEntry(req.params.id)
+                .then(entries => res.json(entries))
+                .catch((err) => {
+                    Logger.error(err);
+                    res.sendStatus(500).send('Fetching entries failed!');
+                })
+        );
     },
-    getEntries
+    getEntries,
+    getEntry
 };

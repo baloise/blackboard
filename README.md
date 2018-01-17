719bbde14d7a7d6889843f251ce9ebbb1537b599
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2018-01-17 15:08:12 +0100

    Initial solr instance

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Blackboard
+
+For the client and the server, you need to have node.js (>= 8.4.0) installed.
+
+### Installing node.js:
+
+- [Node.js](http://nodejs.org) >= 8.4.0
+
+
+### Installing Solar
+
+Get Solr from [Solr](https://lucene.apache.org/solr/mirrors-solr-latest-redir.html) >= 7.2
+
+Running Solr: 
+
+```
+$ ${PATH_TO_EXTRACTED_SOLR}bin\solr start -f -s ${PATH_TO_THIS_GIT_REPO}\server\solr
+```
--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
+const baseUrl = 'http://localhost:8983/solr';
+const collection = 'test';
 
 const server = app.listen(process.env.PORT || 3000, () => {
+    fetch(`${baseUrl}/${collection}/select?q=*:*`)
+        .then(res => res.json())
+        .then(res => console.log(res.response.docs));
+
     console.log(`Listening on port ${server.address().port}`);
 });

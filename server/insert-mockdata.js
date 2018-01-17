9c14ea9e77257bddcc7a6726bf1a12a9bd3a5293
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2018-01-17 17:07:12 +0100

    Add functionality to delete all documents and restore them with mock files

---
+++ server/insert-mockdata.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+
+const baseUrl = 'http://localhost:1234/solr';
+const collection = 'Blackboard';
+
+const addMockData = () => {
+    return updateCollection(require('./mock/db').entry);
+};
+
+const updateCollection = (data) => {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'},
+    };
+    return fetch(`${baseUrl}/${collection}/update?commitWithin=1000&overwrite=true`, options)
+        .then(res => res.json())
+        .catch(error => console.error(error));
+};
+
+const deleteAllDocuments = () => {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({delete: {query: "*:*"}, commit: {}}),
+        headers: {'Content-Type': 'application/json'},
+    };
+    return fetch(`${baseUrl}/${collection}/update?commitWithin=1000&overwrite=true`, options)
+        .then(res => res.json())
+        .catch(error => console.error(error));
+};
+
+deleteAllDocuments().then(() =>
+    addMockData().then(() => console.log('Mock data successfully inserted'))
+);
+

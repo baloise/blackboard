725e80c69c23312ccecbb06d8cce31f2d22ce940
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2018-02-15 14:13:31 +0100

    add mkdir commands

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 cd `dirname $0`
 
 # Extract the package
+mkdir www
 tar -xzf package.tgz
 rm package.tgz
 
 # Swap it all around, keeping the previous version aside in case something goes wrong
 rm -rf www_old
+mkdir blubb
 mv www www_old
 mv build www

5c1cf5cbdacc866b1c05057d5743cb43d31f9da6
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2018-02-15 15:56:42 +0100

    fixes

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,10 @@ cd `dirname $0`
 
 # Extract the package
 mkdir www
-touch donald.txt
 tar -xzf package.tgz
 rm package.tgz
 
 # Swap it all around, keeping the previous version aside in case something goes wrong
 rm -rf www_old
-mkdir blubb
 mv www www_old
 mv build www

f40e6ecb5471553c5fb5377ec77b675a2d87f0c8
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2018-02-15 13:54:31 +0100

    Extract file on server

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd `dirname $0`
+
+# Extract the package
+tar -xzf package.tgz
+rm package.tgz
+
+# Swap it all around, keeping the previous version aside in case something goes wrong
+rm -rf www_old
+mv www www_old
+mv build www
